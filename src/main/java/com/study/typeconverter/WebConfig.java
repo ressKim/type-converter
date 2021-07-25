7a831af9cb54a095b04334c7b88a5554f878236c
ressKim <sh21dlat@gmail.com>
2021-07-25 23:20:10 +0900

    formatter 적용하기 stduy

--- src/main/java/com/study/typeconverter/WebConfig.java
+++ src/main/java/com/study/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import com.study.typeconverter.converter.IntegerToStringConverter;
 import com.study.typeconverter.converter.IpPortToStringConverter;
 import com.study.typeconverter.converter.StringToIntegerConverter;
 import com.study.typeconverter.converter.StringToIpPortConverter;
+import com.study.typeconverter.formater.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //우선순위때문에 주석처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
